8c54dc59e77a20cdf3d7a822f8df36dce50aeec4
Lautaro Rocha <lautarovrocha@gmail.com>
2022-11-10 17:53:05 -0300

    looping images from carrusel

--- clase4/src/components/Slider/Slider.jsx
+++ clase4/src/components/Slider/Slider.jsx
@@ -11,10 +11,23 @@ function Slider(props){
         switch(btn){
             case "btn back":
                 setDisplay(display - 1);
+                loopImages();
                 break;
             case "btn nxt":
                 setDisplay(display + 1);
+                loopImages();
                 break;
+        }
+    }
+
+    function loopImages(){
+
+        console.log(props.pics.length)
+        console.log(display)
+        if(display === props.pics.length - 1){
+            setDisplay(0)
+        }else if((display === 0)){
+            setDisplay(props.pics.length - 1)
         }
     }
 
