4a6414df931c39e12c4db222492c9faaf7a9dc60
Lautaro Rocha <lautarovrocha@gmail.com>
2022-11-15 16:48:06 -0300

    responsiveness fixes

--- clase4/src/utilities/Card/Card.jsx
+++ clase4/src/utilities/Card/Card.jsx
@@ -4,14 +4,11 @@ import './card.css'
 
 function Card(props){
         return(
-            <div className="card">
-            <span className="card-title">{props.obj.title}</span>
-            <img src={props.obj.img} alt="" className="card-img" />
-            <span className="card-price">{props.obj.price}</span>
-            <Link to={`/productos/producto/${props.obj.id}`}>
-                <button>ver detalle</button>
+            <Link to={`/productos/producto/${props.obj.id}`}className="card">
+                <span className="card-title">{props.obj.title}</span>
+                <img src={props.obj.img} alt="" className="card-img" />
+                <span className="card-price">{props.obj.price}</span>
             </Link>
-        </div>
         )
     }
        
